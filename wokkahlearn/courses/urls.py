a94abb809408184dc53819eaf5d98103d26052e0
salako07 <salako.olamide2001@gmail.com>
2025-06-30 07:56:20 +0100

    Push

--- wokkahlearn/courses/urls.py
+++ wokkahlearn/courses/urls.py
@@ -16,23 +16,3 @@ router.register(r'enrollments', CourseEnrollmentViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 ]
-
-
-
-# code_execution/urls.py
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import (
-    ExecutionEnvironmentViewSet, CodeExecutionViewSet, 
-    TestCaseViewSet, CodePlaygroundViewSet
-)
-
-router = DefaultRouter()
-router.register(r'environments', ExecutionEnvironmentViewSet)
-router.register(r'executions', CodeExecutionViewSet)
-router.register(r'test-cases', TestCaseViewSet)
-router.register(r'playground', CodePlaygroundViewSet)
-
-urlpatterns = [
-    path('', include(router.urls)),
-]
